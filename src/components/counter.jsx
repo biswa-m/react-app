7aeddf70258c3eb1d1c92f0038b87cec638b39d9
Biswajit Mahanta <bis.mhnta@gmail.com>
2019-05-04 21:11:17 +0530

    Add dynamic class

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -8,10 +8,16 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <span className={this.getBadgeClass()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
+  }
+
+  getBadgeClass() {
+    let badgeClass = "badge m-2 badge-";
+    badgeClass += this.state.count ? "primary" : "warning";
+    return badgeClass;
   }
 
   formatCount() {
