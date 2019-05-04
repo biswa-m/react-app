0d2b223c76e4d71612a882bbe8bda7ea070a1cb7
Biswajit Mahanta <bis.mhnta@gmail.com>
2019-05-04 20:46:05 +0530

    Render dynamic data using state and method

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,8 +1,24 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0,
+    imageUrl: "https://picsum.photos/200"
+  };
+
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <div>
+        <img src={this.state.imageUrl} />
+        <h1>{this.formatCount()}</h1>
+        <button>Increment</button>
+      </div>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
